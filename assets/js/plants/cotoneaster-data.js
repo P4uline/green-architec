7171693f2c433acf17da7126287bbce912a198db
P4uline <pauline.iogna@gmail.com>
2016-09-17 12:26:34 +0200

    first commit

---
+++ assets/js/plants/cotoneaster-data.js
@@ -0,0 +1,14 @@
+var cotoneaster = {
+    jan:"fruit-rouge-feuille-foncé.png",
+    fev:"fruit-rouge-feuille-foncé.png",
+    mar:"feuille-foncé.png",
+    avr:"feuille-foncé.png",
+    mai:"feuille-foncé.png",
+    jui:"feuille-foncé.png",
+    jul:"feuille-foncé.png",
+    aou:"feuille-foncé.png",
+    sep:"feuille-foncé.png",
+    oct:"feuille-foncé.png",
+    nov:"fruit-rouge-feuille-foncé.png",
+    dec:"fruit-rouge-feuille-foncé.png"
+};
