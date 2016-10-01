12c998e455a669d06b05c1cf66cebdde8a2aca2b
P4uline <pauline.iogna@gmail.com>
2016-10-01 16:06:55 +0200

    use method gridStackItem - move all plants in json object

--- assets/js/green-architect.js
+++ assets/js/green-architect.js
@@ -16,7 +16,7 @@ var plants = {
             "assets/images/fruit-rouge-feuille-fonce.png",
             "assets/images/fruit-rouge-feuille-fonce.png"]
     },
-    "cornus-jaune" : {
+    "cornus-panache-vert-jaune" : {
         name:"Cornus à fauillage vert et jaune panache",
         season:["assets/images/branche-rouge.png",
             "assets/images/feuille-verte-panache-jaune.png",
@@ -29,7 +29,148 @@ var plants = {
             "assets/images/branche-rouge.png",
             "assets/images/branche-rouge.png",
             "assets/images/branche-rouge.png",
-            "assets/images/branche-rouge.png"]
+            "assets/images/branche-rouge.png"
+        ]
+    },
+    "viorne-obier" : {
+        name: "Viorne obier",
+        season: ["assets/images/branche.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille-automne.png",
+            "assets/images/feuille-automne.png",
+            "assets/images/branche.png",
+            "assets/images/branche.png",
+            "assets/images/branche.png",
+            "assets/images/branche.png"
+        ]
+    },
+    "aubepine" : {
+        name:"Aubépine",
+        season:["assets/images/branche.png",
+            "assets/images/fleur-blanche.png",
+            "assets/images/fleur-blanche.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/fruit-rouge.png",
+            "assets/images/fruit-rouge.png",
+            "assets/images/fruit-rouge-branche.png",
+            "assets/images/fruit-rouge-branche.png",
+            "assets/images/fruit-rouge-branche.png",
+            "assets/images/fruit-rouge-branche.png"
+        ]
+    },
+    "noisetier" : {
+        name:"Noisetier",
+        season:["assets/images/branche-chaton.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/branche-chaton.png",
+            "assets/images/branche-chaton.png",
+            "assets/images/branche-chaton.png",
+            "assets/images/branche-chaton.png"]
+    },
+    "prunellier" : {
+        name:"Prunellier",
+        season : ["assets/images/fleur-blanche.png",
+            "assets/images/fleur-blanche.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/fruit-bleu.png",
+            "assets/images/fruit-bleu.png",
+            "assets/images/fruit-bleu-branche.png",
+            "assets/images/fruit-bleu-branche.png",
+            "assets/images/fruit-bleu-branche.png",
+            "assets/images/fruit-bleu-branche.png"]
+    },
+    "groseiller-a-fleur":{
+        name:"Groseiller à fleurs",
+        season:["assets/images/branche.png",
+            "assets/images/fleur-rose.png",
+            "assets/images/fleur-rose.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/branche.png",
+            "assets/images/branche.png",
+            "assets/images/branche.png",
+            "assets/images/branche.png"]
+    },
+    "fusain-europe":{
+        name:"Fusain d'Europe",
+        season:["assets/images/branche.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/feuille.png",
+            "assets/images/fruit-rouge-automne.png",
+            "assets/images/fruit-rouge-automne.png",
+            "assets/images/fruit-rouge-branche.png",
+            "assets/images/fruit-rouge-branche.png",
+            "assets/images/fruit-rouge-branche.png",
+            "assets/images/fruit-rouge-branche.png"
+        ]
+    },
+    "troene-dore":{
+        name:"Troene doré",
+        season:["assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png",
+            "assets/images/feuille-jaune.png"]
+    },
+    "photinia":{
+        name:"Photinia",
+        season:["assets/images/feuille-verte-et-rouge.png",
+            "assets/images/feuille-verte-et-rouge.png",
+            "assets/images/fleur-blanche.png",
+            "assets/images/feuille-verte-et-rouge.png",
+            "assets/images/feuille-verte-et-rouge.png",
+            "assets/images/feuille-verte-et-rouge.png",
+            "assets/images/feuille-verte-et-rouge.png",
+            "assets/images/feuille-verte-et-rouge.png",
+            "assets/images/feuille-verte-et-rouge.png",
+            "assets/images/feuille-verte-et-rouge.png",
+            "assets/images/feuille-verte-et-rouge.png",
+            "assets/images/feuille-verte-et-rouge.png"
+        ]
+    },
+    "eleagnus-ebbingei":{
+        name:"Eleagnus ebbingei",
+        season:["assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png",
+            "assets/images/feuille-fonce.png"
+        ]
     }
 }
 
@@ -41,4 +182,20 @@ function getPlantRow(plant) {
         $row.append($plant.append('<img src=' + plant.season[i] +' alt="feuillage" height="30" width="30"/>'));
     }
     return $row;
+}
+
+function getTitleRow(name) {
+    var $title = $('<div/>', { 'class':'col-md-12' });
+    $title.append(name);
+
+    var $row = $('<div/>', { 'class':'row' });
+    $row.append($title);
+    return $row;
+}
+
+function getGristackItem(name) {
+    var plant = getPlantRow(plants[name]);
+    var title = getTitleRow(plants[name].name);
+    var $content = $('<div class="grid-stack-item-content" />').append(title).append(plant);
+    return $('<div></div>').append($content);
 }
